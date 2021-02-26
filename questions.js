9b5b350fcc049c6a1ba160fdc6e03a299712701c
Javiistacks <javiann.marrero@icloud.com>
2021-02-25 23:06:22 -0600

    upload working game

---
+++ questions.js
@@ -0,0 +1,59 @@
+var questionBank = [
+    {   
+        question: "What belongs to you, but is used by others?",
+        selection: ["your sweater", "your car", "your name", "your socks"],
+        answer: "your name"
+    },
+    {   
+        question: "How many sides has a circle?",
+        selection: ["One", "Two", "None", "I don't know"],
+        answer: "Two"
+    },
+    {   
+        question: "What wont run long without winding?", 
+        selection: ["A airplane", "A river", "propellers", "i don't know"],
+        answer: "A river"
+    },
+    {
+        question: "What has to be broken before you can use it",
+        selection: ["A egg", "your socks", "a box", "nuts"],
+        answer: "A egg"
+    },
+    {
+        question: "What goes up and never goes down?",
+        selection: ["your blood pressure", "superman", "taxes", "your age"],
+        answer: "your age"
+    },
+    {
+        question: "What has legs but cant walk?",
+        selection: ["A cheetah", "A dog", "A chair", "a table"],
+        answer: "a table"
+    },
+    {
+        question: "People make me, save me, change me, and raise me. What am I?",
+        selection: ["A baby", "A dog", "Money", "A painting"],
+        answer: "Money"
+    },
+    {
+        question: "What has hands but cannot clap?",
+        selection: ["A clock", "A T-Rex", "A Monkey", "A baby"],
+        answer: "A clock"
+    },
+    {
+        question: "We're five little things of an everyday sort; you'll find us all in 'a tennis court' ",
+        selection: ["tennis balls", "vowels", "rackets", "Serena Williams"],
+        answer: "vowels"
+    },
+    {
+        question: "What is always on its way here, but never arrives?",
+        selection: ["A boat", "Amazon packages", "An airplane", "Tomorrow"],
+        answer: "Tomorrow"
+    },
+    {
+        question: "I get wet when drying. I get dirty when wiping. What am i?",
+        selection: ["A car", "Kitchen table", "A towel", "I don't know"],
+        answer: "A towel"
+    }
+
+];
+
